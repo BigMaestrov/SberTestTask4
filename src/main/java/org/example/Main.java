558ebbaee331604ec4145949755fdd5d2a710ec2
BigMaestrov <nikita.egorov40k@gmail.com>
2023-06-08 16:42:03 +0400

    Выполнил тестовое задание 2.

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -12,12 +12,32 @@ import java.util.*;
 public class Main {
     public static void main(String[] args) {
         try {
+            Scanner scanner = new Scanner(System.in);
             //Начало отсчета времени
             long time = System.currentTimeMillis();
             //Заполнения списка для вывода информации
             ArrayList<City> cityList = parseCSV("./src/main/resources/data.csv");
             //Конец отсчета времени
             System.out.println((System.currentTimeMillis() - time) + " мс" + ", Найдено строк: " + cityList.size());
+            //Выбор сортировки
+            System.out.println("Выберите тип сортировки"+"\n"+"1.По наименования города"+"\n"+"2.По федеральному округу и наименованию города"+"\n"+">>");
+            String userEnter=scanner.nextLine();
+            switch (userEnter){
+                case("1"):
+                    // Используем компаратор, игнорирующий регистр
+                    Comparator<City> comparator = Comparator.comparing(City::getName, String.CASE_INSENSITIVE_ORDER);
+                    // Сортируем список
+                    Collections.sort(cityList, comparator);
+                    break;
+                case("2"):
+                    // Создаем компаратор, сравнивающий объекты по полю district, а затем по полю name
+                    Comparator<City> comparator1 = Comparator
+                            .comparing(City::getDistrict)
+                            .thenComparing(City::getName);
+                    // Сортируем список
+                    Collections.sort(cityList, comparator1);
+                    break;
+            }
             //Вывод списка
             for (City city : cityList) {
                 System.out.println(city.toString());
@@ -27,7 +47,7 @@ public class Main {
         }
     }
 
-    //Парсинг cdr файла
+    //Парсинг csv файла
     private static ArrayList<City> parseCSV(String filePath) throws IOException {
         ArrayList<City> cityList = new ArrayList<City>();
         //Загрузка строк из файла
